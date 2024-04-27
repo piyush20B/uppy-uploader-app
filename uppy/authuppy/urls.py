4d60c9624f7e6a3bfc95d7ea7a0c106dfdfb5621
piyush0431 <piyush31032005@gmail.com>
2024-04-27 23:25:48 +0530

    updateit

--- uppy/authuppy/urls.py
+++ uppy/authuppy/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signup, name='signup'),
-    path('', views.login, name='login'),
-    path('', views.logout, name='logout'),
-    path('', views.forgetPass, name='home'),
-    path('', views.resetPass, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('forgot-passwd/', views.forgetPass, name='home'),
+    path('reset-passwd/', views.resetPass, name='home'),
 ]
