082ff3fc999ef2c4c84bdfd6776117fa68d18351
piyush0431 <piyush31032005@gmail.com>
2024-04-27 23:30:31 +0530

    commit It!!

--- graphyy.py
+++ graphyy.py
@@ -3,7 +3,7 @@ from random import randint
 
 for i in range(1, 10):
     for j in range(0, randint(1, 10)):
-        d = str(i) + ' days ago'
+        d = str(i) + ' days ago\n'
         with open('file.txt', 'a') as file:
             file.write(d)
         os.system('git add .')
