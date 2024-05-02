d56975285eb08b016daa530f93171fe17b37eaac
pixelThreader <162052026+piyush20B@users.noreply.github.com>
2024-05-03 03:32:28 +0530

    Update urls.py
    added usage app

--- uppy/uppy/urls.py
+++ uppy/uppy/urls.py
@@ -4,11 +4,13 @@ from authuppy import urls as authurls
 from uppyApp import urls as home
 from uploads import urls as uploadsurls
 from terms import urls as termsurls
+from usages import urls as useurls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include(authurls)),
     path('', include(home)),
     path('upload/', include(uploadsurls)),
+    path('usage/', include(useurls)),
     path('terms/', include(termsurls)),
 ]
