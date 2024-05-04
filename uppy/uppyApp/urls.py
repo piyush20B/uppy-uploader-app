4ff70f76a5b9847ac60ebd1d34a1d5011abea6f5
piyush0431 <piyush31032005@gmail.com>
2024-05-05 00:41:31 +0530

    commit 0.2

--- uppy/uppyApp/urls.py
+++ uppy/uppyApp/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('ultra-search/', views.search_ultra, name='ultra-search'),
+    path('search/', views.search, name='search'),
     path('about/', views.about, name='about'),
     path('viewer/', views.viewer, name='viewer'),
     path('contact/', views.contact, name='contact'),
+    path('favourites/', views.favourites, name='favourites'),
+    path('settings/', views.settings, name='settings'),
+    path('profile/<str:user>/', views.profile, name='profile'),
 ]
--- uppy/uppyApp/views.py
+++ uppy/uppyApp/views.py
@@ -3,14 +3,32 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def search_ultra(request, query):
+    process = query
+    return render(request, 'home/index.html')
+
+def search(request, query):
+    process = query
+    return render(request, 'home/index.html')
+
 def index(request):
     return render(request, 'home/index.html')
 
 def about(request):
     return render(request, 'home/about.html')
 
+def favourites(request):
+    return render(request, 'home/favourites.html')
+
+def profile(request, user):
+    context = {'user': user}
+    return render(request, 'home/profile.html', context=context)
+
 def contact(request):
     return render(request, 'home/contact.html')
+
+def settings(request):
+    return render(request, 'home/settings.html')
 
 def viewer(request):
     return render(request, 'home/viewer.html')
