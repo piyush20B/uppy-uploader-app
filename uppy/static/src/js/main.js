8a95af4d289750f182a4a590fd1e4b06431b417a
piyush0431 <rana.workslance@gmail.com>
2024-05-05 03:04:13 +0530

    update things!!

--- uppy/static/src/js/main.js
+++ uppy/static/src/js/main.js
@@ -1,9 +1,15 @@
+/* 
+* JavaScript code for the Uppy project - a cloud storage platform
+* This code is part of a portfolio project
+* Version: 1.0.0
+* Creator: Piyush Rana
+*/
+
 $(document).ready(function () {
-    // Selecting elements
     const button = document.querySelector('#profile-dropdown-toggle');
     const tooltip = document.querySelector('#profile-dropdown');
 
-    // Creating Popper instance
+    // Creating Popper instance for header dropdown positioning...
     const popperInstance = Popper.createPopper(button, tooltip, {
         placement: 'bottom-start',
         modifiers: [
@@ -16,19 +22,19 @@ $(document).ready(function () {
         ],
     });
 
-    // Function to close the dropdown
+    // Function to close the profile dropdown...
     function closeDropdown() {
         $('#profile-dropdown').attr('isOpen', 'closed').stop(true, true).slideUp(300).fadeOut(300);
     }
 
-    // Click event to close dropdown when profile link is clicked
+    // Click event to close dropdown when profile link is clicked...
     $('.link-profile-nav').click(function () {
         closeDropdown();
     });
 
-    // Click event to toggle dropdown
+    // Click event to toggle profile dropdown...
     $('#profile-dropdown-toggle').click(function () {
-        popperInstance.update(); // Update Popper position
+        popperInstance.update(); // Update Popper position...
         const isOpen = $('#profile-dropdown').attr('isOpen');
         if (isOpen === "closed") {
             $('#profile-dropdown').attr('isOpen', 'opened').stop(true, true).slideDown(300).fadeIn(300);
@@ -37,17 +43,17 @@ $(document).ready(function () {
         }
     });
 
-    // Click event to toggle menu
-    // This is necessary animation for my toggler
+    // Click event to toggle menu...
     $('.toggle-menu').click(function () {
         const openMenu = $('.toggle-menu .open-menu');
         if (openMenu.attr('isOpen') === "closed") {
+            // Open menu animations...
             openMenu.addClass('active').attr('isOpen', "opened");
-            $('body').addClass('ovh')
-            $('.nav-wrapper').fadeIn(300).css('display', 'flex');
-            $('header').addClass('active');
-            $('.nav-main-container').animate({ opacity: 1, transform: 'scale(1)' }, 500);
-            // Animate li items using GSAP
+            $('body').addClass('ovh'); // Add class to prevent scrolling...
+            $('.nav-wrapper').fadeIn(300).css('display', 'flex'); // Fade in navigation wrapper and display it as flex...
+            $('header').addClass('active'); // Add class to header for styling purposes...
+            $('.nav-main-container').animate({ opacity: 1, transform: 'scale(1)' }, 500); // Animate scaling and opacity...
+            // Animate li items using GSAP...
             gsap.to('.nav-item', {
                 duration: 0.5,
                 opacity: 1,
@@ -55,12 +61,13 @@ $(document).ready(function () {
                 stagger: 0.1
             });
         } else if (openMenu.attr('isOpen') === "opened") {
+            // Close menu animations...
             openMenu.removeClass('active').attr('isOpen', "closed");
-            $('body').removeClass('ovh')
-            $('.nav-wrapper').fadeOut(300);
-            $('header').removeClass('active');
-            $('.nav-main-container').animate({ opacity: 0, transform: 'scale(0.7)' }, 500);
-            // Animate li items using GSAP
+            $('body').removeClass('ovh'); // Remove class to enable scrolling...
+            $('.nav-wrapper').fadeOut(300); // Fade out navigation wrapper...
+            $('header').removeClass('active'); // Remove class from header...
+            $('.nav-main-container').animate({ opacity: 0, transform: 'scale(0.7)' }, 500); // Animate scaling and opacity
+            // Animate li items using GSAP...
             gsap.to('.nav-item', {
                 duration: 0.5,
                 opacity: 0,
