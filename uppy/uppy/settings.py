3d5db60827fa450d74bb874cb09a7b8d2530d280
piyush0431 <rana.workslance@gmail.com>
2024-05-05 13:11:07 +0530

    update Changes!!!

--- uppy/uppy/settings.py
+++ uppy/uppy/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'usages.apps.UsagesConfig',
     'uploads.apps.UploadsConfig',
     'chat.apps.ChatConfig',
+    'blogs.apps.BlogsConfig',
     'terms.apps.TermsConfig',
 ]
 
--- uppy/uppy/urls.py
+++ uppy/uppy/urls.py
@@ -6,12 +6,14 @@ from uploads import urls as uploadsurls
 from terms import urls as termsurls
 from usages import urls as useurls
 from chat import urls as chaturls
+from blogs import urls as blogurls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include(authurls)),
     path('', include(home)),
     path('usage/', include(useurls)),
+    path('uppy-info/', include(blogurls)),
     path('upload/', include(uploadsurls)),
     path('chat/', include(chaturls)),
     path('terms/', include(termsurls)),
